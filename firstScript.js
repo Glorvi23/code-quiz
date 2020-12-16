736dac77ca9b408545415f97223ed7c7b38d5487
Caleb Glorvigen <jcglorvi@gmail.com>
2020-12-15 22:07:33 -0500

    Updated the README.md

--- firstScript.js
+++ firstScript.js
@@ -7,17 +7,3 @@ firstNewEl.textContent = "Commonly used data types DO NOT Include:";
 
 firstCodingTitle.append(firstNewEl);
 
-setTime();
-
-// We only want to start the timer if totalSeconds is > 0
-if (totalSeconds > 0) {
-    /* The "interval" variable here using "setInterval()" begins the recurring increment of the
-       secondsElapsed variable which is used to check if the time is up */
-    interval = setInterval(function () {
-        secondsElapsed++;
-
-        // So renderTime() is called here once every second.
-        renderTime();
-    }, 1000);
-
-}
